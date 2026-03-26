64717f609c17dcdfecf3baf32540c9da01e4f181
Juan Pablo Díaz <diazperillaj@gmail.com>
2026-03-26 14:28:02 -0500

    Added QR page to redirect

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
 import ShayaCafe    from './features/ShayaCafe/ShayaCafe.tsx'
 import CheckoutPage from './features/ShayaCafe/sub_pages/CheckoutPage.tsx'
@@ -17,6 +17,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/"         element={<ShayaCafe />} />
         <Route path="/checkout" element={<CheckoutPage />} />
         <Route path="/nosotros"   element={<AboutUs />} />
+        <Route path="/qr"   element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
